2b462d4f086f667f809d90b2e5b1f01f392707f0
neyron357-boop <neyron357@gmail.com>
2026-05-11 02:07:26 +0400

    orders: open order details directly from list tap

--- dubai-spares-cis/screens/OrdersScreen.tsx
+++ dubai-spares-cis/screens/OrdersScreen.tsx
@@ -692,7 +692,7 @@ const OrdersScreen: React.FC = () => {
                     toggleOrderSelected(order.id);
                     return;
                   }
-                  navigate(`/order/${order.id}`);
+                  openOrderPreview(order);
                 }}
                 disableCardTap={!!deleteId || isDeleting}
               >
